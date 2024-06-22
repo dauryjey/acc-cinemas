ef9e26417ef17a302ff259f7ae8e40453644ae9a
Daury P. <dauryjey@gmail.com>
2024-06-22 18:12:34 -0400

    chore: Remove username field from User model and userRepo

--- backend/src/repos/userRepo.ts
+++ backend/src/repos/userRepo.ts
@@ -1,19 +1,22 @@
 import { prisma } from "../utils/db"
 import bcrypt from "bcrypt"
 import { User } from "@prisma/client"
+import {
+  PrismaUniqueConstraintError,
+  toTypedPrismaError,
+} from "../utils/prismaErrors"
 
 async function createUser(user: User): Promise<User> {
-  const { email, firstName, lastName, username, password, isAdmin } = user
+  const { email, firstName, lastName, password, isAdmin } = user
 
   const hashedPassword = await bcrypt.hash(password, 10)
-	
+
   try {
     const newUser = await prisma.user.create({
       data: {
         email,
         firstName,
         lastName,
-        username,
         password: hashedPassword,
         isAdmin,
       },
@@ -21,8 +24,26 @@ async function createUser(user: User): Promise<User> {
 
     return newUser
   } catch (error) {
+    if (toTypedPrismaError(error) instanceof PrismaUniqueConstraintError) {
+      throw new Error("Email already exists")
+    }
+
     throw new Error("Error creating user")
   }
 }
 
+async function getUserByEmail(email: string): Promise<User | null> {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
 
+    return user
+  } catch (error) {
+    throw new Error("Error getting user by email")
+  }
+}
+
+export default { createUser, getUserByEmail } as const
