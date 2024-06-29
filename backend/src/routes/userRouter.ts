40c3ad13adfc891528ac58e2b0e704caaf3eca4e
Daury P. <dauryjey@gmail.com>
2024-06-28 21:42:52 -0400

    refactor: formatted code

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -3,6 +3,12 @@ import UserController from "../controllers/UserController"
 
 export const userRouter = Router()
 
-userRouter.post("/signup", async (req, res) => UserController.createUser(req, res))
-userRouter.post("/login", async (req, res) => UserController.loginUser(req, res))
-userRouter.get(":email", async (req, res) => UserController.getUserByEmail(req, res))
+userRouter.post("/signup", async (req, res) =>
+  UserController.createUser(req, res)
+)
+userRouter.post("/login", async (req, res) =>
+  UserController.loginUser(req, res)
+)
+userRouter.get(":email", async (req, res) =>
+  UserController.getUserByEmail(req, res)
+)
