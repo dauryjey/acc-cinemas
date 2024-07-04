a8720212eda6de2361e5de0f0f6123f3ef053a93
Daury P. <dauryjey@gmail.com>
2024-07-04 18:19:16 -0400

    refactor: Update ticketRouter to include token validation and authorization middleware

--- backend/src/routes/ticketRouter.ts
+++ backend/src/routes/ticketRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import TicketController from "../controllers/TicketController"
+import { isAdmin, validateToken } from "../middlewares/jwtMiddleware"
 
 export const ticketRouter = Router()
 
-ticketRouter.get("/all", TicketController.getTickets)
-ticketRouter.get("/:id", TicketController.getTicketById)
-ticketRouter.post("/create", TicketController.createTicket)
-ticketRouter.put("/:id", TicketController.updateTicket)
-ticketRouter.delete("/:id", TicketController.deleteTicket)
+ticketRouter.get("/all", validateToken, isAdmin, TicketController.getTickets)
+ticketRouter.get("/:id", validateToken, TicketController.getTicketById)
+ticketRouter.post("/create", validateToken, TicketController.createTicket)
+ticketRouter.put("/:id", validateToken, isAdmin, TicketController.updateTicket)
+ticketRouter.delete("/:id", validateToken, isAdmin, TicketController.deleteTicket)
