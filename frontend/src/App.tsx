7e6c69d4b1bce9c4f45a785d14f04f0738106704
Daury P. <dauryjey@gmail.com>
2024-07-05 22:18:00 -0400

    chore: Add frontend boilerplate and dependencies

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Card, CardBody } from "@nextui-org/react"
+
+function App() {
+  return (
+    <Card>
+      <CardBody>
+        <h1>Hello, ACC!</h1>
+      </CardBody>
+    </Card>
+  )
+}
+
+export default App
